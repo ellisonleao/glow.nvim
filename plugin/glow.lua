8300f517e0d6cffb242f6bce7005219b0f9a3f05
vinibispo <vini.bispo015@gmail.com>
2022-06-26 11:03:45 -0300

    feat(glow): add command for GlowString

--- plugin/glow.lua
+++ plugin/glow.lua
@@ -1,3 +1,4 @@
 -- create install cmd
 vim.cmd("command! GlowInstall :lua require('glow').download_glow()")
 vim.cmd("command! -nargs=? -complete=file Glow :lua require('glow').glow('<f-args>')")
+vim.cmd("command! -nargs=1 GlowString lua require('glow').glow_string('<args>')")
